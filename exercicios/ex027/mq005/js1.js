4ec644fd91df3545a56acd6d11869de8a4f104cf
Marcello121 <200999459+Marcello121@users.noreply.github.com>
2025-04-30 17:08:27 -0300

    ex 027 mq005

---
+++ exercicios/ex027/mq005/js1.js
@@ -0,0 +1,12 @@
+function clickMenu(){
+
+    // Se o style display do menu for igual a 'none', ou seja, se estiver escondido ele vai transformar o display em block, OU SEJA, vai mostrar o menu.
+    if (menu.style.display == 'none'){
+    
+        menu.style.display = 'block';
+    
+    } else {
+    menu.style.display = 'none';
+    }
+
+}
